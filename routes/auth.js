f89f5462a05d6cd89e9a66b1f5a9fd83e928163b
taiki-engineer <taiki.m0209@outlook.jp>
2026-07-04 19:37:48 +0900

    JWT追加

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const { body, validationResult } = require("express-validator");
 const { User } = require("../db/user");
 const bcrypt = require("bcrypt");
+const JWT = require("jsonwebtoken");
 
 router.get("/", (req, res) => {
     res.send("Hello AuthJs");
@@ -43,8 +44,59 @@ router.post("/register",
         });
 
         // ６，クライアントへJWTの発行
+        const token = await JWT.sign({
+            email,
+        },
+        "SECRET_KEY",
+        {
+            expiresIn: "24h",
+        }
+        );
+
+        return res.json({
+            token: token,
+        });
     }
 );
+
+// ログイン用のAPI
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = User.find((user) => user.email === email);
+    if (!user) {
+        return res.status(400).json([
+            {
+                message: "すでにそのユーザーは存在ません",
+            },
+        ]);
+    }
+
+    // パスワードの複合、照合
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+        return res.status(400).json([
+            {
+                message: "パスワードが異なります",
+            },
+        ]);
+    }
+
+    const token = await JWT.sign({
+        email,
+    },
+    "SECRET_KEY",
+    {
+        expiresIn: "24h",
+    }
+    );
+
+    return res.json({
+        token: token,
+    });
+
+})
 
 // DBのユーザー確認するAPI
 router.get("/allUser", (req, res) => {
